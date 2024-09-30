4c1e4ececb90509664569b1a5f22e4944c70ab01
Diego Carlos <diegcarlos@gmail.com>
2024-09-30 15:09:52 -0400

    "Updated file permissions, added react-native-usb-thermal-printer dependency, and declared its types in index.d.ts"

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,11 @@ declare module '*.png' {}
 declare module '*.jpg' {}
 declare module '*.mp4' {}
 
+declare module 'react-native-usb-thermal-printer' {
+  export function onPrintDeviceList(): any;
+  export function onPrintClear(id);
+  export function onPrintCut(id: any, line: number, beep: boolean);
+  export function onPrintImageBase64();
+  export function onPrintImageURL();
+  export function onPrintText();
+}
