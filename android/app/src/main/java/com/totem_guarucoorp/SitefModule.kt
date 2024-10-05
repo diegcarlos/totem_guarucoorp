510c004180cdec2e46ca5905619a4ef1e81b6a4b
joao01sb <bonfimnew@gmail.com>
2024-10-04 22:03:30 -0300

    feat: add Sitef AAR payment method and configuration

--- android/app/src/main/java/com/totem_guarucoorp/SitefModule.kt
+++ android/app/src/main/java/com/totem_guarucoorp/SitefModule.kt
@@ -1,27 +1,57 @@
 package com.totem_guarucoorp
 
-import br.com.testes.sitef.SitefPag
+import br.com.pagamento.sitefpay.model.CallBackEvent
+import br.com.pagamento.sitefpay.pagamentoservice.SitefPay
+import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
-
-
+import java.math.BigDecimal
 
 
 class SitefModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    private val sitefInstance: SitefPag = SitefPag()
+    private val sitefInstance: SitefPay = SitefPay(currentActivity!!)
 
     override fun getName(): String {
         return "SitefPag"
     }
 
     @ReactMethod
+    fun configurarSitef() {
+        try {
+            TODO("Configurações do msitef")
+//            sitefInstance.configuraMsitef()
+        } catch (e: Exception) {
+
+        }
+    }
+
+    @ReactMethod
+    fun cancelarTransacao(valor: BigDecimal, callback: CallBackEvent) {
+        try {
+            sitefInstance.cancelarTransacao(valor, callback)
+        } catch (e: Exception) {
+
+        }
+    }
+
+    @ReactMethod
+    fun transacoesGerenciaisConfig(callback: CallBackEvent) {
+        try {
+            sitefInstance.transacoesGerenciais(callback)
+        } catch (e: Exception) {
+
+        }
+    }
+
+    @ReactMethod
     fun pagar(promise: Promise) {
         try {
-            val result = sitefInstance.pagar()
-            promise.resolve(result)
+            TODO("Todo pagamento")
+//            val result = sitefInstance.pagar()
+//            promise.resolve(result)
         } catch (e: Exception) {
             promise.reject("Erro ao chamar o método pagar", e)
         }
